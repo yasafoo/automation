c408271b015ddf46d93d8ce9dbbaab7a243d86e8
Yasafoo <Yasafoo@Yasafoo-PC>
2014-11-01 22:52:19 +0530

    Added Salesforcelogin page and Modified login logics.

---
+++ Automation/src/superadmin/SalesForceLogin.java
@@ -0,0 +1,50 @@
+package superadmin;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.How;
+import org.openqa.selenium.support.PageFactory;
+
+public class SalesForceLogin {
+	private WebDriver driver;
+	
+	@FindBy(how=How.ID,using="username")
+	private WebElement txtUserName;
+	
+	@FindBy(how=How.ID,using="password")
+	private WebElement txtPassword;
+	
+	@FindBy(how=How.ID,using="Login")
+	private WebElement btnLogin;
+	
+	public SalesForceLogin(WebDriver driver){
+		this.driver = driver;
+		PageFactory.initElements(driver, this);
+	}
+
+	public WebElement getTxtUserName() {
+		return txtUserName;
+	}
+
+	public void setTxtUserName(WebElement txtUserName) {
+		this.txtUserName = txtUserName;
+	}
+
+	public WebElement getTxtPassword() {
+		return txtPassword;
+	}
+
+	public void setTxtPassword(WebElement txtPassword) {
+		this.txtPassword = txtPassword;
+	}
+
+	public WebElement getBtnLogin() {
+		return btnLogin;
+	}
+
+	public void setBtnLogin(WebElement btnLogin) {
+		this.btnLogin = btnLogin;
+	}
+	
+}
--- Automation/src/superadmin/SuperAdminTest.java
+++ Automation/src/superadmin/SuperAdminTest.java
@@ -12,16 +12,26 @@ public class SuperAdminTest {
 	WebDriver driver;
 	SuperAdminLogin sal;
 	SuperAdminHome sah;
+	SalesForceLogin sfl;
 	
 	@Before
 	public void setUp(){
 		driver = new FirefoxDriver();
 		driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
-		driver.get("https://corpadmin:monpass321@tst2-super.leapset.com/login.php");
+		driver.get("https://test.salesforce.com/");
+		//driver.get("https://corpadmin:monpass321@tst2-super.leapset.com/login.php");
 	}
 	
 	@Test
 	public void setupAMerchant(){
+		sfl = new SalesForceLogin(driver);
+		sfl.getTxtUserName().clear();
+		sfl.getTxtUserName().sendKeys("alerts-qatwo@leapset.com.full");
+		sfl.getTxtPassword().clear();
+		sfl.getTxtPassword().sendKeys("qwertyu1");
+		sfl.getBtnLogin().click();
+		//driver.close();
+		driver.get("https://corpadmin:monpass321@tst2-super.leapset.com/login.php");
 		sal = new SuperAdminLogin(driver);
 		sal.getTxtLogin().clear();
 		sal.getTxtLogin().sendKeys("admin");
